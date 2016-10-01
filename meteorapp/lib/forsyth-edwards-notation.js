73cd0d02d57e233ad2605192395f221bec93a6d9
Dirk Stolle <striezel-dev@web.de>
2016-10-02 01:31:30 +0200

    add function to get Forsythe-Edwards notation (FEN) of a board

--- meteorapp/lib/forsyth-edwards-notation.js
+++ meteorapp/lib/forsyth-edwards-notation.js
@@ -53,6 +53,12 @@ FEN = {
   */
   toBoard: function(fen)
   {
+    //This function currently only handles the first part, i.e. the pieces on the board.
+    var parts = fen.split(' ');
+    if (parts.length > 1)
+    {
+      fen = parts[0];
+    }
     var rows = fen.split('/');
     if (rows.length < 8)
       return null;
@@ -90,5 +96,141 @@ FEN = {
       bRow = bRow - 1;
     } //for i
     return board;
+  },
+
+
+  /* fromBoard: returns a FEN for the board with the given ID
+
+     parameters:
+       boardId - (string) ID of the board
+
+     return value:
+       Returns a string that contains the board's FEN on success.
+       Returns null, if an error occurred (e.g. non-existent board).
+
+     remarks:
+       This function is still very slow.
+       TODO: Improve speed by reducing number of DB queries.
+  */
+  fromBoard(boardId)
+  {
+    let boardDoc = Boards.findOne({_id: boardId});
+    if (!boardDoc)
+      return null;
+
+    let emptyCount = 0;
+    let fenString = "";
+    for (r = 8; r >= 1; --r)
+    {
+      for (c = 'a'; c <= 'h'; c = String.fromCharCode(c.charCodeAt(0)+1))
+      {
+        let fieldDoc = Fields.findOne({board: boardId, column: c, row: r});
+        if (fieldDoc.piece === 'empty')
+          ++emptyCount;
+        else
+        {
+          //First, add empty count, if any.
+          if (emptyCount > 0)
+          {
+            fenString = fenString + emptyCount;
+            emptyCount = 0;
+          }
+          //now add the real thing
+          switch(fieldDoc.colour)
+          {
+            case 'white':
+                 switch(fieldDoc.piece)
+                 {
+                   case 'rook':
+                        fenString = fenString + 'R';
+                        break;
+                   case 'knight':
+                        fenString = fenString + 'N';
+                        break;
+                   case 'bishop':
+                        fenString = fenString + 'B';
+                        break;
+                   case 'queen':
+                        fenString = fenString + 'Q';
+                        break;
+                   case 'king':
+                        fenString = fenString + 'K';
+                        break;
+                   case 'pawn':
+                        fenString = fenString + 'P';
+                        break;
+                   default:
+                        //invalid piece
+                        return null;
+                 } //switch piece
+                 break;
+            case 'black':
+                 switch(fieldDoc.piece)
+                 {
+                   case 'rook':
+                        fenString = fenString + 'r';
+                        break;
+                   case 'knight':
+                        fenString = fenString + 'n';
+                        break;
+                   case 'bishop':
+                        fenString = fenString + 'b';
+                        break;
+                   case 'queen':
+                        fenString = fenString + 'q';
+                        break;
+                   case 'king':
+                        fenString = fenString + 'k';
+                        break;
+                   case 'pawn':
+                        fenString = fenString + 'p';
+                        break;
+                   default:
+                        //invalid piece
+                        return null;
+                 } //switch piece
+                 break;
+          } //switch colour
+        } //else
+      } //for columns
+      //First, add empty count, if any.
+      if (emptyCount > 0)
+      {
+        fenString = fenString + emptyCount;
+        emptyCount = 0;
+      }
+      //add slash - except after last row
+      if (r > 1)
+        fenString = fenString + "/";
+    } //for rows
+    //Who is to move?
+    switch(boardDoc.toMove)
+    {
+      case 'white':
+           fenString = fenString + " w";
+           break;
+      case 'black':
+           fenString = fenString + " b";
+           break;
+      default:
+           //Something is wrong here, return without extra data.
+           return fenString;
+    } //switch
+    //castling information
+    let castlingString = "";
+    if (boardDoc.castling.white.kingside)
+      castlingString = castlingString + "K";
+    if (boardDoc.castling.white.queenside)
+      castlingString = castlingString + "Q";
+    if (boardDoc.castling.black.kingside)
+      castlingString = castlingString + "k";
+    if (boardDoc.castling.black.queenside)
+      castlingString = castlingString + "q";
+    if (castlingString !== "")
+      fenString = fenString + " " + castlingString;
+    else
+      fenString = fenString + " -";
+    //return here, because other data is not implemeted yet
+    return fenString;
   }
 };
