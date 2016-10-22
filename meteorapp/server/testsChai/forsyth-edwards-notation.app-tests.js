ebfcb20f4a5e638fca674e777cb293890c75eb2d
Dirk Stolle <striezel-dev@web.de>
2016-10-22 15:18:15 +0200

    improve test for FEN.toBoard() and increase timeout
    The test sometimes runs for more than 2000 ms, so the timeout
    had to be increased.

--- meteorapp/server/testsChai/forsyth-edwards-notation.app-tests.js
+++ meteorapp/server/testsChai/forsyth-edwards-notation.app-tests.js
@@ -16,6 +16,8 @@ describe('Forsyth-Edwards Notation tests', function () {
   */
 
   it("FEN.toBoard('<starting position>')", function () {
+    //set timeout for this test to 5 seconds, because it might take a while
+    this.timeout(5000);
     //insert WWTP with synchronous call
     var boardID = FEN.toBoard('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
     //ID should be a string
@@ -38,5 +40,29 @@ describe('Forsyth-Edwards Notation tests', function () {
     // ... and 8 black pawns in row 7.
     fields = Fields.find({board: boardID, colour: 'black', piece: 'pawn', row: 7});
     expect(fields.count()).to.equal(8);
+    //check white pieces
+    fields = Fields.find({board: boardID, colour: 'white', row: 1}, {sort: {column: 1}});
+    expect(fields.count()).to.equal(8);
+    fields = fields.fetch();
+    expect(fields[0].piece).to.equal('rook');
+    expect(fields[1].piece).to.equal('knight');
+    expect(fields[2].piece).to.equal('bishop');
+    expect(fields[3].piece).to.equal('queen');
+    expect(fields[4].piece).to.equal('king');
+    expect(fields[5].piece).to.equal('bishop');
+    expect(fields[6].piece).to.equal('knight');
+    expect(fields[7].piece).to.equal('rook');
+    //check black pieces
+    fields = Fields.find({board: boardID, colour: 'black', row: 8}, {sort: {column: 1}});
+    expect(fields.count()).to.equal(8);
+    fields = fields.fetch();
+    expect(fields[0].piece).to.equal('rook');
+    expect(fields[1].piece).to.equal('knight');
+    expect(fields[2].piece).to.equal('bishop');
+    expect(fields[3].piece).to.equal('queen');
+    expect(fields[4].piece).to.equal('king');
+    expect(fields[5].piece).to.equal('bishop');
+    expect(fields[6].piece).to.equal('knight');
+    expect(fields[7].piece).to.equal('rook');
   });
 });
