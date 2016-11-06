652400dce75bd3d1b1cfb21498ecbe95b5d1c751
Dirk Stolle <striezel-dev@web.de>
2016-11-06 13:17:49 +0100

    add introduction page

--- meteorapp/routes/routes.js
+++ meteorapp/routes/routes.js
@@ -2,11 +2,9 @@ Router.configure({
   layoutTemplate: 'main'
 });
 
-Router.route('/', {name: 'boardSelection'});
-
+Router.route('/', {name: 'intro'});
+Router.route('/select', {name: 'boardSelection'});
 Router.route('/board', {name: 'board'});
 Router.route('/board/delete', {name: 'boardDelete'});
-
 Router.route('/settings', {name: 'settings'});
-
 Router.route('/info', {name: 'information'});
